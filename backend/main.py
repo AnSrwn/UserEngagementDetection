bb08ad13b5d60ded75cbd86723f3ba2322a9f58e
AnSrwn <an.srwn@gmail.com>
2023-06-12 15:23:33 +0200

    wip: webRTC implementation

--- backend/main.py
+++ backend/main.py
@@ -1,8 +1,21 @@
+import argparse
+import logging
+import asyncio
+import ssl
+import uuid
+import json
+
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
 from database.models import Test
 from database.database import engine, create_db_and_tables
 from routers import test
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
+from pydantic import BaseModel
+
+logger = logging.getLogger("pc")
+pcs = set()
 
 app = FastAPI(title="UserEngagementDetection")
 
@@ -10,7 +23,7 @@ app = FastAPI(title="UserEngagementDetection")
 #We define authorizations for middleware components
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:8000"],
+    allow_origins=["http://localhost:8000", "http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
@@ -28,3 +41,54 @@ app.include_router(test.router)
 @app.get("/")
 async def root():
     return {"message": "UserEngagementDetection API is working."}
+
+class OfferRequest(BaseModel):
+    sdp: str
+    type: str
+
+# WebRTC
+@app.post("/offer")
+async def offer(request: OfferRequest):
+    logger.info(request)
+    offer = RTCSessionDescription(sdp=request.sdp, type=request.type)
+
+    pc = RTCPeerConnection()
+    pc_id = "PeerConnection(%s)" % uuid.uuid4()
+    pcs.add(pc)
+
+    def log_info(msg, *args):
+        logger.info(pc_id + " " + msg, *args)
+
+    @pc.on("connectionstatechange")
+    async def on_connectionstatechange():
+        log_info("Connection state is %s", pc.connectionState)
+        if pc.connectionState == "failed":
+            await pc.close()
+            pcs.discard(pc)
+
+    @pc.on("track")
+    def on_track(track):
+        log_info("Track %s received", track.kind)
+
+        # if track.kind == "video":
+            # pc.addTrack(
+            #     VideoTransformTrack(
+            #         relay.subscribe(track), transform=params["video_transform"]
+            #     )
+            # )
+            # if args.record_to:
+            #     recorder.addTrack(relay.subscribe(track))
+
+        @track.on("ended")
+        async def on_ended():
+            log_info("Track %s ended", track.kind)
+            # await recorder.stop()
+
+    # handle offer
+    await pc.setRemoteDescription(offer)
+
+    # send answer
+    answer = await pc.createAnswer()
+    await pc.setLocalDescription(answer)
+
+    return pc.localDescription
--- frontend/pages/detection/index.vue
+++ frontend/pages/detection/index.vue
@@ -1,7 +1,187 @@
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+
+// TODO: Selection of camera
+// TODO: Handling change of camera
+// TODO: Start and Stop buttons
+// TODO: Stop on page change
+
+// WebRTC
+const dataChannel = ref(null)
+
+// peer connection
+var peerConnection = null;
+
+// data channel
+var dc = null, dcInterval = null;
+
+function createPeerConnection() {
+    var config = {
+        sdpSemantics: 'unified-plan'
+    };
+
+    config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
+
+    peerConnection = new RTCPeerConnection(config);
+
+    // register some listeners to help debugging
+    const iceGatheringState = document.getElementById('iceGatheringState');
+    peerConnection.addEventListener('icegatheringstatechange', function () {
+        iceGatheringState.textContent += ' -> ' + peerConnection.iceGatheringState;
+    }, false);
+    iceGatheringState.textContent = peerConnection.iceGatheringState;
+
+    const iceConnectionState = document.getElementById('iceConnectionState');
+    peerConnection.addEventListener('iceconnectionstatechange', function () {
+        iceConnectionState.textContent += ' -> ' + peerConnection.iceConnectionState;
+    }, false);
+    iceConnectionState.textContent = peerConnection.iceConnectionState;
+
+    const signalingState = document.getElementById('signalingState');
+    peerConnection.addEventListener('signalingstatechange', function () {
+        signalingState.textContent += ' -> ' + peerConnection.signalingState;
+    }, false);
+    signalingState.textContent = peerConnection.signalingState;
+
+    // connect audio / video
+    // pc.addEventListener('track', function(evt) {
+    //     if (evt.track.kind == 'video')
+    //         document.getElementById('video').srcObject = evt.streams[0];
+    //     else
+    //         document.getElementById('audio').srcObject = evt.streams[0];
+    // });
+
+    return peerConnection;
+}
+
+async function negotiate() {
+    const offer = await peerConnection.createOffer()
+    await peerConnection.setLocalDescription(offer)
+
+    const offerSdp = document.getElementById('offerSdp');
+    offerSdp.textContent = offer.sdp;
+
+    // wait for ICE gathering to complete
+    // await new Promise(function (resolve) {
+    //     if (pc.iceGatheringState === 'complete') {
+    //         resolve();
+    //     } else {
+    //         function checkState() {
+    //             if (pc.iceGatheringState === 'complete') {
+    //                 pc.removeEventListener('icegatheringstatechange', checkState);
+    //                 resolve();
+    //             }
+    //         }
+    //         pc.addEventListener('icegatheringstatechange', checkState);
+    //     }
+    // })
+
+    let response = await $fetch('offer', {
+        method: 'POST',
+        baseURL: 'http://localhost:8000',
+        body: offer,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+    }).catch((error) => {
+        alert(error);
+    })
+
+    const answerSdp = document.getElementById('answerSdp');
+    answerSdp.textContent = response.sdp;
+    peerConnection.setRemoteDescription(response);
+}
+
+async function start() {
+    document.getElementById('start').style.display = 'none';
+
+    peerConnection = createPeerConnection();
+
+    try {
+        const constraints = { 'video': true, 'audio': false };
+        // video: {
+        // width: {min: 640, ideal: 1280, max: 1920},
+        // height: {min: 480, ideal: 720, max: 1080}
+        // }
+        const stream = await navigator.mediaDevices.getUserMedia(constraints);
+        stream.getTracks().forEach(function (track) {
+            peerConnection.addTrack(track, stream);
+        })
+
+        const videoElement = document.querySelector('video#localVideo');
+        videoElement.srcObject = stream;
+
+        await negotiate();
+        console.log(peerConnection)
+    } catch (error) {
+        console.error('Error opening video camera.', error);
+    }
+
+    document.getElementById('stop').style.display = 'inline-block';
+}
+
+function stop() {
+    document.getElementById('stop').style.display = 'none';
+    document.getElementById('start').style.display = 'inline-block';
+
+    // close data channel
+    if (dc) {
+        dc.close();
+    }
+
+    // close transceivers
+    if (peerConnection.getTransceivers) {
+        peerConnection.getTransceivers().forEach(function (transceiver) {
+            if (transceiver.stop) {
+                transceiver.stop();
+            }
+        });
+    }
+
+    // close local audio / video
+    peerConnection.getSenders().forEach(function (sender) {
+        sender.track.stop();
+    });
+
+    // close peer connection
+    setTimeout(function () {
+        peerConnection.close();
+    }, 500);
+}
+
+</script>
 
 <template>
-    <h1>Detection</h1>
+    <div>
+        <h1>Detection</h1>
+        <div>
+            <video id="localVideo" autoplay playsinline controls="false" />
+        </div>
+
+        <button id="start" @click="start()">Start</button>
+        <button id="stop" style="display: none" @click="stop()">Stop</button>
+
+
+        <h2>State</h2>
+        <p>
+            ICE gathering state: <span id="iceGatheringState"></span>
+        </p>
+        <p>
+            ICE connection state: <span id="iceConnectionState"></span>
+        </p>
+        <p>
+            Signaling state: <span id="signalingState"></span>
+        </p>
+        <h2>Data channel</h2>
+        <pre ref="dataChannel" style="height: 200px;"></pre>
+        <h2>SDP</h2>
+
+        <h3>Offer</h3>
+        <pre id="offerSdp"></pre>
+
+        <h3>Answer</h3>
+        <pre id="answerSdp"></pre>
+    </div>
 </template>
 
 <style lang='scss'></style>
