e8d4ace6b51156256f3df43c5e082765bf2afe77
AnSrwn <an.srwn@gmail.com>
2023-06-19 22:35:54 +0200

    implement basic engagement detection

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,9 @@
 import logging
 import uuid
+import keras
+import cv2
+import dlib
+import numpy
 
 from fastapi import FastAPI, HTTPException
 from fastapi.logger import logger
@@ -17,8 +21,11 @@ log = logging.getLogger('uvicorn.debug')
 app = FastAPI(title="UserEngagementDetection")
 log.info("FastAPI started")
 
+engagementModel = keras.models.load_model('models/parallel_model.h5')
+log.info("Engagement Model loaded")
 
-#We define authorizations for middleware components
+
+# We define authorizations for middleware components
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
@@ -77,7 +84,9 @@ async def offer(request: OfferRequest):
 
         if track.kind == "video":
             # return same video
-            pc.addTrack(track)
+            pc.addTrack(VideoTransformTrack(
+                relay.subscribe(track)
+            ))
 
         @track.on("ended")
         async def on_ended():
@@ -94,3 +103,54 @@ async def offer(request: OfferRequest):
     log.info("LocalDescription: " + str(pc.localDescription))
 
     return pc.localDescription
+
+# A video stream track that transforms frames from an another track.
+class VideoTransformTrack(MediaStreamTrack):
+
+    kind = "video"
+
+    def __init__(self, track):
+        super().__init__()  # don't forget this!
+        self.track = track
+
+    async def recv(self):
+        frame = await self.track.recv()
+        image = frame.to_ndarray(format="bgr24")
+
+        try:
+            detector = dlib.get_frontal_face_detector()
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            faces = detector(gray)
+
+            roi = []
+            for face in faces:
+                x1 = face.left()
+                y1 = face.top()
+                x2 = face.right()
+                y2 = face.bottom()
+                # if no face skip predictions
+                if len(image[y1:y2, x1:x2]) <= 0 or len(image[y1-100:y2+100, x1-100:x2+100]) <= 0:
+                    log.info('No face detected')
+                    return frame
+                # append faces
+                roi.append(cv2.resize(cv2.cvtColor(image[y1:y2, x1:x2], cv2.COLOR_BGR2GRAY), (48,48)))
+                # get predictions
+                predictions = []
+                if len(roi)>0:
+                    test_images = numpy.expand_dims(roi, axis=3)
+                    predictions = engagementModel.predict(test_images)
+
+                    boredom = round(predictions[0][0][1],3)
+                    engagement = round(predictions[1][0][1],3)
+                    confusion = round(predictions[2][0][1],3)
+                    frustration = round(predictions[3][0][1],3)
+
+                    log.info(f"Boredem: {boredom} | Engagement: {engagement} | Confusion: {confusion} | Frustration: {frustration}")
+
+        except UnboundLocalError as e:
+            # if no predictions
+            log.error(f"No predictions: {e}")
+        except Exception as e:
+            log.error(e)
+        
+        return frame
