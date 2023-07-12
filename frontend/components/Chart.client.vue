ea83edd19e201c491812cb67d2061538ba795691
AnSrwn <an.srwn@gmail.com>
2023-07-12 22:18:18 +0200

    update simple chart with live data

--- frontend/components/Chart.client.vue
+++ frontend/components/Chart.client.vue
@@ -7,97 +7,25 @@ import { line, curveCatmullRom } from "d3-shape";
 import { scaleLinear, scaleUtc } from "d3-scale";
 import { axisBottom, axisLeft } from "d3-axis";
 import { timeFormat } from "d3-time-format";
-import { select, create } from "d3-selection";
+import { select, selectAll } from "d3-selection";
 import { extent, max } from "d3-array";
 import { ref, defineExpose } from "vue";
 
-function createChart(data) {
-  // Declare the chart dimensions and margins.
-  const width = 928;
-  const height = 500;
-  const marginTop = 20;
-  const marginRight = 30;
-  const marginBottom = 30;
-  const marginLeft = 40;
+// Declare the chart dimensions and margins.
+// set the dimensions and margins of the graph
+const margin = { top: 10, right: 30, bottom: 30, left: 50 };
+const width = 460 - margin.left - margin.right;
+const height = 400 - margin.top - margin.bottom;
 
-  // Declare the x (horizontal position) scale.
-  const x = scaleUtc(
-    extent(data, (d) => d.time),
-    [marginLeft, width - marginRight]
-  );
+let svg = null;
+let x = null;
+let y = null;
+let xAxis = null;
+let yAxis = null;
 
-  // Declare the y (vertical position) scale.
-  const y = scaleLinear(
-    [0, max(data, (d) => d.engagement)],
-    [height - marginBottom, marginTop]
-  );
-
-  // Declare the line generator.
-  const lineGenerator = line()
-    .x((d) => x(d.time))
-    .y((d) => y(d.engagement));
-  // .curve(curveCatmullRom.alpha(0.5));
-
-  // Create the SVG container.
-  const svg = create("svg").attr("width", width).attr("height", height);
-  // .attr("viewBox", [0, 0, width, height])
-  // .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
-
-  // Add the x-axis.
-  var format = timeFormat("%X");
-  svg
-    .append("g")
-    .attr("class", "x")
-    .attr("transform", `translate(0,${height - marginBottom})`)
-    .call(
-      axisBottom(x).tickFormat(format)
-      // .ticks(width / 80)
-      // .tickSizeOuter(0)
-    );
-
-  // Add the y-axis, remove the domain line, add grid lines and a label.
-  svg
-    .append("g")
-    .attr("class", "y")
-    .attr("transform", `translate(${marginLeft},0)`)
-    .call(axisLeft(y).ticks(height / 40))
-    .call((g) => g.select(".domain").remove())
-    .call((g) =>
-      g
-        .selectAll(".tick line")
-        .clone()
-        .attr("x2", width - marginLeft - marginRight)
-        .attr("stroke-opacity", 0.1)
-    )
-    .call((g) =>
-      g
-        .append("text")
-        .attr("x", -marginLeft)
-        .attr("y", 10)
-        .attr("fill", "currentColor")
-        .attr("text-anchor", "start")
-        .text("↑ Engagement")
-    );
-
-  // Append a path for the line.
-  svg
-    .append("path")
-    .datum(data)
-    .attr("fill", "none")
-    .attr("stroke", "steelblue")
-    .attr("stroke-width", 1.5)
-    .attr("class", "line")
-    .attr("d", lineGenerator(data));
-
-  return svg.node();
-}
-
-let isInitial = true;
 const chartDiv = ref(null);
-// defineExpose({ chartDiv })
 let chartData = ref([]);
 
-// const { chartData } = defineProps(['chartData'])
 const props = defineProps({
   data: Array,
 });
@@ -108,118 +36,78 @@ onMounted(() => {
     (newValue, oldValue) => {
       chartData.value = newValue;
 
-      if (newValue !== undefined && newValue.length) {
-        // const plot = Plot.plot({
-        //   grid: true,
-        //   marks: [
-        //     Plot.frame(),
-        //     Plot.ruleY([0]),
-        //     Plot.line(newValue, { x: "time", y: "engagement" }),
-        //   ],
-        // });
-
-        // const plot = createChart(newValue);
-        // chartDiv.value.append(plot);
-
-        if (isInitial) {
-          isInitial = false;
-          const plot = createChart(newValue);
-          chartDiv.value.append(plot);
-        } else {
-          // Declare the x (horizontal position) scale.
-          const x = scaleUtc(
-            extent(data, (d) => d.time),
-            [marginLeft, width - marginRight]
-          );
-
-          // Declare the y (vertical position) scale.
-          const y = scaleLinear(
-            [0, max(data, (d) => d.engagement)],
-            [height - marginBottom, marginTop]
-          );
-
-          // Create the X axis:
-          x.domain([
-            0,
-            d3.max(data, function (d) {
-              return d.ser1;
-            }),
-          ]);
-          svg.selectAll(".myXaxis").transition().duration(3000).call(xAxis);
-
-          // create the Y axis
-          y.domain([
-            0,
-            d3.max(data, function (d) {
-              return d.ser2;
-            }),
-          ]);
-          svg.selectAll(".myYaxis").transition().duration(3000).call(yAxis);
-
-          // TODO: https://plnkr.co/edit/ZEQlzJtHKcVdcAWmuwxA?p=preview&preview
-          const plot = select("#myplot").transition();
-          plot
-            .select("line")
-            .duration(750)
-            .attr(
-              "d",
-              line(newValue)
-                .x((d) => x(d.time))
-                .y((d) => y(d.engagement))
-            );
-        }
+      if (newValue.length) {
+        updateChart(newValue);
       }
     }
   );
 });
 
-// export default {
-//   props: {
-//     chartData: Array,
-//   },
-//   // data() {
-//   //     return {
-//   //       engagementData: []
-//   //     }
-//   // },
-//   watch: {
-//     chartData: {
-//       handler(newValue, oldValue) {
-//         console.log(newValue);
-//         // this.engagementData = newValue;
-//         if (newValue !== undefined && newValue.length) {
-//           // const plot = Plot.plot({
-//           //   grid: true,
-//           //   marks: [
-//           //     Plot.frame(),
-//           //     Plot.ruleY([0]),
-//           //     Plot.line(newValue, { x: "time", y: "engagement" }),
-//           //   ],
-//           // });
+function onchartDivMounted() {
+  // append the svg object to the body of the page
+  svg = select("#myplot")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-//           if (isInitial) {
-//             isInitial = false;
-//             const plot = createChart(newValue);
-//             const div = document.querySelector("#myplot");
-//             div.append(plot);
-//           } else {
-//             console.log("hello");
-//             // TODO: https://plnkr.co/edit/ZEQlzJtHKcVdcAWmuwxA?p=preview&preview
-//             const plot = select("#myplot").transition();
-//             plot.select(".line").duration(750).attr("d", line(newValue));
-//           }
-//         }
-//       },
-//       deep: true,
-//     },
-//   },
-// };
+  // Initialise a X axis:
+  var format = timeFormat("%X");
+  x = scaleUtc().range([0, width]);
+  xAxis = axisBottom().scale(x).tickFormat(format);
+  svg
+    .append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .attr("class", "myXaxis");
+
+  // Initialize an Y axis
+  y = scaleLinear().range([height, 0]);
+  yAxis = axisLeft().scale(y);
+  svg.append("g").attr("class", "myYaxis");
+}
+
+function updateChart(data) {
+  // Create the X axis:
+  x.domain(extent(data, (d) => d.time));
+  svg.selectAll(".myXaxis").transition().duration(500).call(xAxis);
+
+  // create the Y axis
+  y.domain([0, max(data, (d) => d.engagement)]);
+  svg.selectAll(".myYaxis").transition().duration(500).call(yAxis);
+
+  // Create a update selection: bind to the new data
+  var u = svg.selectAll(".line").data([data], function (d) {
+    return d.time;
+  });
+
+  // Updata the line
+  u.enter()
+    .append("path")
+    .attr("class", "line")
+    .merge(u)
+    .transition()
+    .duration(1000)
+    .attr(
+      "d",
+      line()
+        .x(function (d) {
+          return x(d.time);
+        })
+        .y(function (d) {
+          return y(d.engagement);
+        })
+    )
+    .attr("fill", "none")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 1.5);
+}
 </script>
 
 <template>
   <div>
-    <div>Engagement: {{ chartData }}</div>
-    <div ref="chartDiv" id="myplot"></div>
+    <h2>Engagement:</h2>
+    <div @vue:mounted="onchartDivMounted" ref="chartDiv" id="myplot"></div>
   </div>
 </template>
 
