24b0a4e330a34bb87cd972c4c9572596b1b5baa8
AnSrwn <an.srwn@gmail.com>
2023-07-14 11:31:52 +0200

    production dockerfile for frontend

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,7 +1,7 @@
 import { resolve } from "path";
 
 export default defineNuxtConfig({
-  devtools: { enabled: true },
+  devtools: { enabled: false },
   runtimeConfig: {
     public: {
       baseURL: process.env.BASE_URL || 'http://localhost:8000',
