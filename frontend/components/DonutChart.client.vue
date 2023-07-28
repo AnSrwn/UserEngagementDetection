c22e2dda54dc45c51fb0d907c7fda1cea0325ac4
AnSrwn <an.srwn@gmail.com>
2023-07-28 17:42:33 +0200

    smooth transition for donut chart

--- frontend/components/DonutChart.client.vue
+++ frontend/components/DonutChart.client.vue
@@ -1,32 +1,35 @@
 <!-- Composition API -->
 <script setup>
 import { transition } from "d3-transition";
-import { line, arc, pie } from "d3-shape";
-import { scaleLinear, scaleUtc, scaleOrdinal } from "d3-scale";
-import { axisBottom, axisLeft } from "d3-axis";
-import { timeFormat } from "d3-time-format";
+import { interpolate } from "d3-interpolate";
+import { arc, pie } from "d3-shape";
+import { scaleOrdinal } from "d3-scale";
 import { select } from "d3-selection";
-import { extent, max } from "d3-array";
 import { ref } from "vue";
 import { v4 as uuidv4 } from "uuid";
 
-// Declare the chart dimensions and margins.
-// set the dimensions and margins of the graph
 const margin = 40;
 const width = 450;
 const height = 450;
 
 const radius = Math.min(width, height) / 2 - margin;
+const innerRadius = 120;
+const cornerRadius = 8;
+const padAngle = 0.01; // marign betwenn segments
+const transitionDuration = 2000;
 const color = scaleOrdinal().range(["#0aa849", "#e3df0b", "#a11f0b"]);
 
-let svg = null;
-let chartData = ref({});
+const chartUuid = uuidv4();
 
 const props = defineProps({
   data: Object,
 });
 
-const chartUuid = uuidv4();
+let svg = null;
+let gElement = null;
+let chartPie = null;
+let chartArc = null;
+let chartData = ref({});
 
 function onNewValue(newValue) {
   if (newValue === null) newValue = {};
@@ -36,14 +39,28 @@ function onNewValue(newValue) {
 }
 
 function onChartDivMounted() {
-  // append the svg object to the body of the page
+  // Setup donut chart
   svg = select(`#donut_chart-${chartUuid}`)
     .append("svg")
     .attr("width", width)
-    .attr("height", height)
+    .attr("height", height);
+
+  gElement = svg
     .append("g")
     .attr("transform", `translate(${width / 2},${height / 2})`);
 
+  chartPie = pie()
+    .startAngle(-(Math.PI / 2) * 5)
+    .value((d) => d[1])
+    .sort(null)
+    .padAngle(padAngle);
+
+  chartArc = arc()
+    .innerRadius(innerRadius)
+    .outerRadius(radius)
+    .cornerRadius(cornerRadius);
+
+  // Handle new data
   onNewValue(props.data);
   watch(
     () => props.data,
@@ -53,49 +70,48 @@ function onChartDivMounted() {
   );
 }
 
+// Store the displayed angles in _current.
+// Then, interpolate from _current to the new angles.
+// During the transition, _current is updated in-place by d3.interpolate.
+function arcTween(a) {
+  var i = interpolate(this._current, a);
+  this._current = i(0);
+  return function (t) {
+    return chartArc(i(t));
+  };
+}
+
 function updateChart(data) {
-  // Compute the position of each group on the pie:
-  var pieChart = pie()
-    .startAngle(-(Math.PI / 2) * 5)
-    .value((d) => d[1])
-    .sort(null)(Object.entries(data));
+  // add transition to new path
+  gElement
+    .datum(Object.entries(data))
+    .selectAll("path")
+    .data(chartPie)
+    .transition()
+    .duration(transitionDuration)
+    .attrTween("d", arcTween);
 
-  // const data_ready = pieChart(Object.entries(data));
-
-  // Create a update selection: bind to the new data
-  var u = svg.selectAll(`.donut_chart-${chartUuid}`).data(pieChart);
-
-  // Updata the line
-  u.enter()
+  // add new data
+  gElement
+    .datum(Object.entries(data))
+    .selectAll("path")
+    .data(chartPie)
+    .enter()
     .append("path")
     .attr("class", `donut_chart-${chartUuid}`)
-    .merge(u)
-    .transition()
-    .duration(2000)
-    .attr(
-      "d",
-      arc()
-        .innerRadius(100) // This is the size of the donut hole
-        .outerRadius(radius)
-    )
     .attr("fill", (d) => color(d.data[0]))
-    .attr("stroke", "black")
-    .style("stroke-width", "2px");
+    .attr("d", chartArc)
+    .each(function (d) {
+      this._current = d;
+    });
 
-  // Build the pie chart: Basically, each part of the pie is a path that we build using the arc function.
-  // svg
-  //   .selectAll(`#donut_chart-${chartUuid}`)
-  //   .data(data_ready)
-  //   .join("path")
-  //   .attr(
-  //     "d",
-  //     arc()
-  //       .innerRadius(100) // This is the size of the donut hole
-  //       .outerRadius(radius)
-  //   )
-  //   .attr("fill", (d) => color(d.data[0]))
-  //   .attr("stroke", "black")
-  //   .style("stroke-width", "2px");
+  // remove data not used
+  gElement
+    .datum(Object.entries(data))
+    .selectAll("path")
+    .data(chartPie)
+    .exit()
+    .remove();
 }
 </script>
 
