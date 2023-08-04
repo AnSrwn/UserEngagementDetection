38b1576f8aedf815f773557bdcb26465d7a69cf9
AnSrwn <an.srwn@gmail.com>
2023-08-04 10:10:40 +0200

    wip: restyle detection page and enable camera change

--- frontend/pages/detection/index.vue
+++ frontend/pages/detection/index.vue
@@ -1,106 +1,126 @@
 <script setup>
-import { ref, onMounted } from "vue";
+import {ref} from "vue";
 
 // TODO: Selection of camera
 // TODO: Handling change of camera
 // TODO: Start and Stop buttons
 // TODO: Stop on page change
 
+const privacyAgreed = ref(false);
+let step = ref(1);
+
 // WebRTC
 const dataChannel = ref(null);
 
 // peer connection
-var peerConnection = null;
+let localPeerConnection = null;
 
 // data channel
-var dc = null,
-  dcInterval = null;
+const dc = null;
 
+let sender = null;
+let stream = null;
 const config = useRuntimeConfig();
 const stunServerUrl = config.public.stunServerUrl;
 
+let selectedCamera = ref(null);
+let cameraList = ref([]);
+
+// Template Refs
+let videoContainer = ref(null);
+let cameraSelection = ref(null);
+
+watch(step, (newValue, oldValue) => {
+      if (newValue === 2) {
+        start();
+      }
+    }
+);
+
+watch(selectedCamera, async (newValue, oldValue) => {
+  if (oldValue !== null && newValue !== oldValue) {
+    await onCameraChange();
+  }
+});
+
+onDeactivated(() => {
+  stop();
+})
+
+onBeforeUnmount(() => {
+  stop();
+})
+
 function createPeerConnection() {
-  var config = {
+  const config = {
     sdpSemantics: "unified-plan",
   };
 
   if (stunServerUrl && stunServerUrl.length > 0) {
-    config.iceServers = [{ urls: [stunServerUrl] }];
+    config.iceServers = [{urls: [stunServerUrl]}];
   }
 
-  // config.iceServers = [{ urls: ['stun:stun.l.google.com:19302'] }];
-  // config.iceServers = [
-  //     {
-  //         urls: ['stun:coturn.andreas-sauerwein.com'],
-  //     },
-  //     {
-  //         urls: ['turn:coturn.andreas-sauerwein.com'],
-  //         username: 'test',
-  //         credential: 'test123'
-  //     }
-  // ];
-
-  peerConnection = new RTCPeerConnection(config);
+  localPeerConnection = new RTCPeerConnection(config);
 
   // register some listeners to help debugging
   const iceGatheringState = document.getElementById("iceGatheringState");
-  peerConnection.addEventListener(
-    "icegatheringstatechange",
-    function () {
-      iceGatheringState.textContent +=
-        " -> " + peerConnection.iceGatheringState;
-    },
-    false
+  localPeerConnection.addEventListener(
+      "icegatheringstatechange",
+      function () {
+        iceGatheringState.textContent +=
+            " -> " + localPeerConnection.iceGatheringState;
+      },
+      false
   );
-  iceGatheringState.textContent = peerConnection.iceGatheringState;
+  iceGatheringState.textContent = localPeerConnection.iceGatheringState;
 
   const iceConnectionState = document.getElementById("iceConnectionState");
-  peerConnection.addEventListener(
-    "iceconnectionstatechange",
-    function () {
-      iceConnectionState.textContent +=
-        " -> " + peerConnection.iceConnectionState;
-    },
-    false
+  localPeerConnection.addEventListener(
+      "iceconnectionstatechange",
+      function () {
+        iceConnectionState.textContent +=
+            " -> " + localPeerConnection.iceConnectionState;
+      },
+      false
   );
-  iceConnectionState.textContent = peerConnection.iceConnectionState;
+  iceConnectionState.textContent = localPeerConnection.iceConnectionState;
 
   const signalingState = document.getElementById("signalingState");
-  peerConnection.addEventListener(
-    "signalingstatechange",
-    function () {
-      signalingState.textContent += " -> " + peerConnection.signalingState;
-    },
-    false
+  localPeerConnection.addEventListener(
+      "signalingstatechange",
+      function () {
+        signalingState.textContent += " -> " + localPeerConnection.signalingState;
+      },
+      false
   );
-  signalingState.textContent = peerConnection.signalingState;
+  signalingState.textContent = localPeerConnection.signalingState;
 
   // connect video
   const videoElement = document.querySelector("video#localVideo");
-  peerConnection.addEventListener("track", function (evt) {
-    if (evt.track.kind == "video") videoElement.srcObject = evt.streams[0];
+  localPeerConnection.addEventListener("track", function (evt) {
+    if (evt.track.kind === "video") videoElement.srcObject = evt.streams[0];
   });
 
-  return peerConnection;
+  return localPeerConnection;
 }
 
 async function negotiate() {
-  const localDescription = await peerConnection.createOffer();
-  await peerConnection.setLocalDescription(localDescription);
-  console.info("LocalDescription set");
+  const localDescription = await localPeerConnection.createOffer();
+  await localPeerConnection.setLocalDescription(localDescription);
+  console.debug("LocalDescription set");
 
   // wait for ICE gathering to complete
   await iceGatheringCompleted();
 
-  const offer = peerConnection.localDescription;
+  const offer = localPeerConnection.localDescription;
 
-  console.info("Offer:");
-  console.info(offer);
+  console.debug("Offer:");
+  console.debug(offer);
   const offerSdp = document.getElementById("offerSdp");
   offerSdp.textContent = offer.sdp;
 
-  console.info("Send offer");
-  let { data: response, error } = await useApiFetch("offer", {
+  console.debug("Send offer");
+  let {data: response, error} = await useApiFetch("offer", {
     method: "POST",
     body: offer,
     headers: {
@@ -114,58 +134,104 @@ async function negotiate() {
 
   response = response.value;
 
-  console.info("Received Answer:");
-  console.info(response);
+  console.debug("Received Answer:");
+  console.debug(response);
   const answerSdp = document.getElementById("answerSdp");
   answerSdp.textContent = response.sdp;
-  peerConnection.setRemoteDescription(response);
+  await localPeerConnection.setRemoteDescription(response);
 }
 
 async function iceGatheringCompleted() {
   return new Promise(function (resolve) {
-    if (peerConnection.iceGatheringState === "complete") {
+    if (localPeerConnection.iceGatheringState === "complete") {
       resolve();
     } else {
       function checkState() {
-        if (peerConnection.iceGatheringState === "complete") {
-          peerConnection.removeEventListener(
-            "icegatheringstatechange",
-            checkState
+        if (localPeerConnection.iceGatheringState === "complete") {
+          localPeerConnection.removeEventListener(
+              "icegatheringstatechange",
+              checkState
           );
           resolve();
         }
       }
-      peerConnection.addEventListener("icegatheringstatechange", checkState);
+
+      localPeerConnection.addEventListener("icegatheringstatechange", checkState);
     }
   });
+}
+
+function getActiveCameraId(stream) {
+  return stream.getVideoTracks()
+      .map((track) => track.getSettings().deviceId)[0];
+}
+
+function getActiveCamera(stream) {
+  const activeDeviceId = getActiveCameraId(stream);
+  return cameraList.value.find((camera) => camera.deviceId === activeDeviceId);
+}
+
+async function getAllCameras() {
+  let devices = await navigator.mediaDevices.enumerateDevices()
+  return devices.filter(device => device.kind === 'videoinput');
+}
+
+async function getStream() {
+  let constraints = {}
+  if (selectedCamera.value !== null) {
+    constraints.video = {deviceId: {exact: selectedCamera.value.deviceId}};
+  } else {
+    constraints.video = true;
+  }
+
+  let newStream = await navigator.mediaDevices.getUserMedia(constraints);
+
+  if (stream !== null) {
+    stream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+  }
+  stream = newStream;
+  return stream;
+}
+
+function getVideoTrack(stream) {
+  let constraints = {
+    frameRate: {
+      ideal: 20.0
+    },
+    facingMode: 'user', // use front camera on mobile devices
+    width: {ideal: 1280},
+    height: {ideal: 960}
+  };
+
+  const [track] = stream.getVideoTracks();
+  track.applyConstraints(constraints);
+  return track;
 }
 
 async function start() {
   document.getElementById("start").style.display = "none";
 
-  peerConnection = createPeerConnection();
+  localPeerConnection = createPeerConnection();
 
   try {
-    // TODO: think of the best framerate
-    const videoConstraints = {
-      video: {
-        frameRate: {
-          exact: 20,
-        },
-        // width: {min: 640, ideal: 1280, max: 1920},
-        // height: {min: 480, ideal: 720, max: 1080}
-      },
-    };
+    stream = await getStream();
+    const track = getVideoTrack(stream);
 
-    const stream = await navigator.mediaDevices.getUserMedia({
-      video: videoConstraints,
-    });
-    stream.getVideoTracks().forEach(function (track) {
-      peerConnection.addTrack(track, stream);
-    });
+    // TODO Maybe rescale track before sending it with WebRTC
+    sender = localPeerConnection.addTrack(track, stream)
+
+
+    if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
+      console.error("enumerateDevices() not supported.");
+    } else {
+      cameraList.value = await getAllCameras();
+      selectedCamera.value = getActiveCamera(stream);
+    }
 
     await negotiate();
-    console.info(peerConnection);
+    console.debug(localPeerConnection);
   } catch (error) {
     console.error("Error opening video camera.", error);
   }
@@ -177,38 +243,72 @@ function stop() {
   document.getElementById("stop").style.display = "none";
   document.getElementById("start").style.display = "inline-block";
 
+  if (stream !== null) {
+    stream.getTracks().forEach(function (track) {
+      track.stop();
+    });
+  }
+
+  videoContainer.value.srcObject = null;
+
   // close data channel
   if (dc) {
     dc.close();
   }
 
   // close transceivers
-  if (peerConnection.getTransceivers) {
-    peerConnection.getTransceivers().forEach(function (transceiver) {
+  if (localPeerConnection.getTransceivers) {
+    localPeerConnection.getTransceivers().forEach(function (transceiver) {
       if (transceiver.stop) {
         transceiver.stop();
       }
     });
   }
 
-  // close local audio / video
-  peerConnection.getSenders().forEach(function (sender) {
-    sender.track.stop();
-  });
-
   // close peer connection
   setTimeout(function () {
-    peerConnection.close();
+    localPeerConnection.close();
   }, 500);
+}
+
+async function onCameraChange() {
+  stream = await getStream()
+  const track = getVideoTrack(stream);
+  await sender.replaceTrack(track);
 }
 </script>
 
 <template>
   <div>
     <h1>Detection</h1>
-    <div>
-      <video id="localVideo" autoplay playsinline controls="false" />
+
+    <!-- Step 1 -->
+    <div v-if="step === 1">
+      <div>Before using this application please read the
+        <NuxtLink target="_blank" to="/privacy">data privacy policy</NuxtLink>
+        . To continue, accept it by clicking on the checkbox.
+      </div>
+      <el-checkbox v-model="privacyAgreed" label="I read and agree to the data privacy policy." size="large"/>
+      <br/>
+      <el-button :disabled="privacyAgreed === false" type="primary" @click="step = 2">Start Video Recording</el-button>
     </div>
+
+    <!-- Step 2 -->
+    <div class="step-2-body">
+      <video id="localVideo" ref="videoContainer" autoplay playsinline/>
+    </div>
+
+    <el-select v-if="cameraList !== null && cameraList.length > 0" ref="cameraSelection" v-model="selectedCamera"
+               class="camera-selection" placeholder="Select" size="large">
+      <el-option
+          v-for="camera in cameraList"
+          :key="camera.deviceId"
+          :label="camera.label"
+          :value="camera"
+      />
+    </el-select>
+
+    <br/>
 
     <button id="start" @click="start()">Start</button>
     <button id="stop" style="display: none" @click="stop()">Stop</button>
@@ -229,4 +329,24 @@ function stop() {
   </div>
 </template>
 
-<style lang='scss'></style>
+<style lang='scss'>
+.step-2-body {
+  display: flex;
+  justify-content: center;
+}
+
+#localVideo {
+  width: 640px;
+  height: 480px;
+  background: gray;
+}
+
+.video-placeholder {
+  height: 480px;
+  width: 640px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: gray;
+}
+</style>
---
+++ frontend/pages/privacy/index.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    <h1>Data Privacy</h1>
+    <p>Hello there</p>
+  </div>
+</template>
+
+<style lang='scss'></style>
